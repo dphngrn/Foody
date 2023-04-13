45f2e2825fc04f60ffb928dea9346e30adf250c6
dphngrn <101129882+dphngrn@users.noreply.github.com>
2023-04-13 19:12:30 +0200

    js

--- Transition.js
+++ Transition.js
@@ -1,12 +1,16 @@
-function transitionDiv(){
-    // get the two divs
-    var div1 = document.getElementById("d1");
-    var div2 = document.getElementById("d2");
 
-    // hide div1 and show div2
-    div1.style.display = "none";
-    div2.style.display = "block";
-
-    // add transition effect
-    div2.style.transition = "all 2s ease-in-out";
+window.onclick = function(event) {
+    let img = document.createElement("img");
+    img.src = "image/fourchette.png";
+    img.style.position = "absolute";
+    img.style.top = event.clientY + "px";
+    img.style.left = event.clientX + "px";
+    img.style.transition = "top 1s ease-in-out";
+    document.body.appendChild(img);
+    setTimeout(function(){
+        img.style.top = (event.clientY + 1000) + "px";
+        setTimeout(function(){
+            document.body.removeChild(img);
+        }, 1000);
+    }, 10);
 }
